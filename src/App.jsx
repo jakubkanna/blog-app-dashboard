275a21f3d7513ddec30ed5bf3649fee36f18e6d9
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-05-28 15:19:16 +0200

    dark-mode

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./styles/App.css";
+import { ThemeToggleBtn } from "./components/ToggleThemeButton";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <ThemeToggleBtn />
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
