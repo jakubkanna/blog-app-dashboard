d7e80bbb56d0778328ee663db5e58dbcb596aa95
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-06-24 14:52:06 +0200

    MUI Alert box added

--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -1,25 +1,32 @@
-import { AdvancedImage } from "@cloudinary/react";
+import { AdvancedImage, placeholder, responsive } from "@cloudinary/react";
 import DropZone from "../components/DropZone";
 import { useEffect, useState } from "react";
 import { Cloudinary } from "@cloudinary/url-gen";
-import { auto } from "@cloudinary/url-gen/actions/resize";
-import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
+import ButtonDelete from "../components/ButtonDelete";
+import { Alert, AlertTitle } from "@mui/material";
 
 export default function Images() {
   const [files, setFiles] = useState([]);
   const [imageList, setImageList] = useState([]);
-  const [message, setMessage] = useState({ upload: "", read_list: "" });
+  const [message, setMessage] = useState({
+    upload_msg: "",
+    read_list_msg: "",
+    severity: "",
+  });
 
-  const cdnTag = "guflpyvn";
+  //config
+  const apiKey = "291738647398191";
+  const presetName = "guflpyvn";
   const cld = new Cloudinary({ cloud: { cloudName: "dzsehmvrr" } });
 
-  const uploadImagesCDN = async () => {
+  //functions
+  const uploadImagesCLOUD = async () => {
     for (const file of files) {
       const data = new FormData();
       data.append("file", file);
-      data.append("api_key", "291738647398191");
-      data.append("upload_preset", "guflpyvn");
-      data.append("tags", cdnTag);
+      data.append("api_key", apiKey);
+      data.append("upload_preset", presetName);
+      data.append("tags", presetName);
 
       try {
         const response = await fetch(
@@ -32,76 +39,94 @@ export default function Images() {
 
         if (!response.ok) {
           const result = await response.json();
-          setMessage((prev) => ({
-            ...prev,
-            upload: `Error uploading image: ${result.error.message}`,
-          }));
+          setMessage({
+            upload_msg: `Error uploading image: ${result.error.message}`,
+            read_list_msg: message.read_list_msg,
+            severity: "error",
+          });
         } else {
-          setMessage((prev) => ({ ...prev, upload: "Upload successful" }));
+          setMessage({
+            upload_msg: "Upload successful",
+            read_list_msg: message.read_list_msg,
+            severity: "success",
+          });
           setFiles([]);
-          await readImagesCDN();
+          await readImagesCLOUD();
         }
       } catch (error) {
         console.error("Error during upload:", error);
-        setMessage((prev) => ({
-          ...prev,
-          upload: `Error during upload: ${error.message}`,
-        }));
+        setMessage({
+          upload_msg: `Error during upload: ${error.message}`,
+          read_list_msg: message.read_list_msg,
+          severity: "error",
+        });
       }
     }
   };
 
-  const readImagesCDN = async () => {
+  const readImagesCLOUD = async () => {
     try {
       const response = await fetch(
-        `https://res.cloudinary.com/dzsehmvrr/image/list/${cdnTag}.json`
+        `https://res.cloudinary.com/dzsehmvrr/image/list/${presetName}.json`
       );
 
       if (!response.ok) {
         const result = await response.json();
         console.error("Error reading images:", result.error.message);
-        setMessage((prev) => ({
-          ...prev,
-          read_list: `Error reading images: ${result.error.message}`,
-        }));
+        setMessage({
+          upload_msg: message.upload_msg,
+          read_list_msg: `Error reading images: ${result.error.message}`,
+          severity: "error",
+        });
       } else {
         const result = await response.json();
         setImageList(result.resources);
       }
     } catch (error) {
       console.error("Error during read:", error);
-      setMessage((prev) => ({
-        ...prev,
-        read_list: `Error during read: ${error.message}`,
-      }));
+      setMessage({
+        upload_msg: message.upload_msg,
+        read_list_msg: `Error during read: ${error.message}`,
+        severity: "error",
+      });
     }
   };
 
   useEffect(() => {
-    readImagesCDN();
+    readImagesCLOUD();
   }, []);
 
   return (
     <>
       <h2>Add Image</h2>
       <DropZone props={{ files, setFiles }} />
-      {message.upload && <div className="upload-message">{message.upload}</div>}
-      {files.length > 0 && <button onClick={uploadImagesCDN}>Upload</button>}
+      {message.upload_msg && (
+        <Alert severity={message.severity}>
+          <AlertTitle> {message.upload_msg}</AlertTitle>
+        </Alert>
+      )}
+      {files.length > 0 && <button onClick={uploadImagesCLOUD}>Upload</button>}
       <h2>Library</h2>
-      {message.read_list && (
-        <div className="library-message">{message.read_list}</div>
+      {message.read_list_msg && (
+        <Alert severity={message.severity}>
+          <AlertTitle> {message.read_list_msg}</AlertTitle>
+        </Alert>
       )}
       <div className="imageLibrary">
         {imageList.map((img) => {
-          const cldImg = cld
-            .image(img.public_id)
-            .format("auto")
-            .quality("auto")
-            .resize(auto().gravity(autoGravity()).width(300).height(300));
+          const cldImg = cld.image(img.public_id).quality("auto");
 
           return (
-            <div className="image" key={img.public_id}>
-              <AdvancedImage cldImg={cldImg} />
+            <div key={img.public_id || img.id} style={{ marginBottom: "20px" }}>
+              <ButtonDelete />
+              <div style={{ width: "200px" }} className="image">
+                <AdvancedImage
+                  style={{ maxWidth: "100%" }}
+                  cldImg={cldImg}
+                  plugins={[responsive()]}
+                />
+              </div>
+              <input type="text" />
             </div>
           );
         })}
